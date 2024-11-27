13abd38fe1b1047ba92cf6f631d2fd6a7fd447d7
natrayansf <natrayan.ramalingam@syncfusion.com>
2024-11-27 14:29:03 +0530

    demo

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -11,6 +11,6 @@ _flutter.buildConfig = {"engineRevision":"a18df97ca57a249df5d8d68cd0820600223ce2
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "539887417"
+    serviceWorkerVersion: "757383258"
   }
 });
